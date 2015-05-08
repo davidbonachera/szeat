8c517fe635523ac97466e389c6b2bc1bb51c8a49
David Jennings <davidjennings73@gmail.com>
2015-05-08 14:40:27 +0800

    upgraded jquery on mvc (homepage only for now)

--- index.php
+++ index.php
@@ -28,7 +28,7 @@
 
 <?php if (isset($_SESSION['error'])) unset($_SESSION['error'],$_SESSION['msg']); ?>
 
-<script src="js/jquery.min.js"></script>
+<script src="js/jquery-1.11.3.min.js"></script>
 <script src="js/bootstrap.min.js"></script>
 <script type="text/javascript" src="js/chosen.jquery.min.js"></script>
 <?php if(file_exists('pages/'.$page.'/local.js')) {
